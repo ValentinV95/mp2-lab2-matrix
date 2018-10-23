69fb397c0261e62b4f02eaa52110ffa985b7a53c
BukarevaArina <arina.bukareva26@gmail.com>
2018-10-23 20:12:28 +0300

    Lab 2 Bukareva Arina

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -7,25 +7,74 @@
 
 #include <iostream>
 #include "utmatrix.h"
+#include <stdlib.h>
 //---------------------------------------------------------------------------
 
 void main()
 {
-  TMatrix<int> a(5), b(5), c(5);
-  int i, j;
+  
+  int i,n, j,t1,t2;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки представления треугольных матриц"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+    setlocale(LC_ALL, "Russian");
+  cout << "Тестирование программ поддержки представления треугольных матриц" << endl;
+   
+ cout << "Введите рамеры матриц(<10000)"<<endl;
+ cin >> n;
+ TMatrix<int> a(n), b(n), c(n);
+ if (n > MAX_MATRIX_SIZE)
+ {
+	 cout<<"Размер матриц превышает максимум"<<endl;
+ }
+ cout << "Выберите способ заполнения матриц:" << endl;
+ cout << "1.Заполнение матрицы рандомными числами" << endl;
+ cout << "2.Использование шаблонной матрицы" << endl;
+ cout << "3.Заполнение матрица пользователем" << endl;
+  cin >> t1;
+  if (t1 == 1)
+  {
+	  for (i = 0; i < n; i++)
+	  
+		  for (j = i; j < n; j++) {
+			  a[i][j] = rand() % 1001;
+			  b[i][j] = rand() % 1001;
+		  }
+		  cout << "a =" << a << endl;
+		  cout << "b =" << b << endl;
+	  
+  }
+  if (t1 == 2)
+  {
+	  for (i = 0; i < n; i++)
+		  for (j = i; j < n; j++) {
+			  a[i][j] = i * 10 + j;
+			  b[i][j] = (i * 10 + j) * 100;
+		  }
+  }
+  if (t1 == 3) 
+  {
+	  cout << "Введите матрицу a" << endl;
+	  cin >> a;
+	  cout << "Введите матрицу b" << endl;
+	  cin >> b;
+  }
+  cout << "Выберите дествия, которое необходимо выполнить с матрицами" << endl;
+  cout << "1.Сложение матриц a+b" << endl;
+  cout<< "2.Вычитание матриц a-b" << endl;
+  cin >> t2;
+  if (t2 == 1) 
+  {
+	  c = a + b;
+	  cout << "Matrix a = " << endl << a << endl;
+	  cout << "Matrix b = " << endl << b << endl;
+	  cout << "Matrix c = a + b" << endl << c << endl;
+  }
+  if (t2 == 2) 
+  {
+	  c = a - b;
+	  cout << "Matrix a = " << endl << a << endl;
+	  cout << "Matrix b = " << endl << b << endl;
+	  cout << "Matrix c = a - b" << endl << c << endl;
+  }
+  
 }
 //---------------------------------------------------------------------------
