9d29b605af105db8f838cb298b8f049fa2d807dc
Retinazzz <nik.gusev567@gmail.com>
2021-01-10 23:44:38 +0300

    11try

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -1,31 +1,40 @@
-// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
-//
-// sample_matrix.cpp - Copyright (c) Гергель В.П. 07.05.2001
-//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (20.04.2015)
-//
-// Тестирование верхнетреугольной матрицы
-
 #include <iostream>
 #include "utmatrix.h"
 //---------------------------------------------------------------------------
 
 void main()
 {
-  TMatrix<int> a(5), b(5), c(5);
-  int i, j;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки представления треугольных матриц"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
+    setlocale(LC_ALL, "Russian");
+    int size;
+    cout << "Введите размер матриц" << endl;
+    cin >> size;
+    TMatrix<int> a(size), b(size), c(2), d(2);
+    int i, j;
+
+    for (i = 0; i < size; i++)
+        for (j = i; j < size; j++)
+        {
+            int q, w;
+            cout << "Введите " << j + 1 << " элемент " << i + 1 << " строки первой матрицы." << endl;
+            cin >> q;
+            cout << "Введите " << j + 1 << " элемент " << i + 1 << " строки второй матрицы." << endl;
+            cin >> w;
+            a[i][j] = q;
+            b[i][j] = w;
+        }
+    c = a + b;
+    cout << "Matrix a = " << endl << a << endl;
+    cout << "Matrix b = " << endl << b << endl;
+    cout << "Matrix c = a + b" << endl << c << endl;
+    d = a - b;
+    cout << "Matrix d = a - b" << endl << d << endl;
+    if (a == b)
     {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
+        cout << "Матрицы a и b равны." << endl;
     }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+    if (a != b)
+    {
+        cout << "Матрицы a и b не равны." << endl;
+    }
 }
-//---------------------------------------------------------------------------
