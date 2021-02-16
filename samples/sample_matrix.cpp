c3897c768a7093c02da8e0c05f19df28bc89fe49
Vlada-K <62649143+Vlada-K@users.noreply.github.com>
2021-02-16 11:10:40 +0300

    Add files via upload

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -12,19 +12,25 @@
 
 int main()
 {
-  TMatrix<int> a(5), b(5), c(5);
+    int n;
+    cout << " vvedite size matrix" << endl;
+    cin >> n;
+  TMatrix<int > a(n), b(n), c(n);
   int i, j;
 
   setlocale(LC_ALL, "Russian");
   cout << "Тестирование программ поддержки представления треугольных матриц"
     << endl;
-
-  for (i = 0; i < 5; i++)
+  cout << " vvedite matrix a" << endl;
+  cin >> a;
+  cout << " vvedite b" << endl;
+  cin >> b;
+ /* for (i = 0; i < 5; i++)
     for (j = i; j < 5; j++ )
     {
       a[i][j] =  i * 10 + j;
       b[i][j] = (i * 10 + j) * 100;
-    }
+    }*/
 
   cout << "Matrix a = " << endl << a << endl;
   cout << "Matrix b = " << endl << b << endl;
